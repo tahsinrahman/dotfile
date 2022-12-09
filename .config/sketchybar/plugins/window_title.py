3b84e966a500ea4d2d1507e8e3bd858dce600c8c
Md Tahsin Rahman <tahsin.rahman@agoda.com>
2022-12-09 14:15:17 +0800

    Use id instead of pid, since same pid can have multiple windows

--- .config/sketchybar/plugins/window_title.py
+++ .config/sketchybar/plugins/window_title.py
@@ -5,22 +5,22 @@ import json
 
 all_apps_cmd = '''
 yabai -m query --windows --space \
-| jq '.[] | {app: .app, pid: .pid, index: ."stack-index", sticky: ."is-sticky", minimized: ."is-minimized" } \
+| jq '.[] | {app: .app, id: .id, index: ."stack-index", sticky: ."is-sticky", minimized: ."is-minimized" } \
 | select(.sticky == false and .minimized == false)' | jq -s 'sort_by(.index)'
 '''
 apps = subprocess.run(all_apps_cmd, capture_output=True, shell=True, text=True).stdout
 apps = json.loads(apps)
 
-current_pid_cmd = '''
-yabai -m query --windows --window | jq '.pid'
+current_id_cmd = '''
+yabai -m query --windows --window | jq '.id'
 '''
-current_pid = subprocess.run(current_pid_cmd, capture_output=True, shell=True, text=True)
-current_pid = int(current_pid.stdout)
+current_id = subprocess.run(current_id_cmd, capture_output=True, shell=True, text=True)
+current_id = int(current_id.stdout)
 
 display = []
 for (idx, app) in enumerate(apps):
     app_name = app["app"]
-    if app["pid"] == current_pid:
+    if app["id"] == current_id:
         app_name = '*' + app_name
     display.append(app_name)
 
